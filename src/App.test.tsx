e6b117b4cdcd4b363d95bd64350ad85024f3327e
Tim Maas <tim.maas@conclusion.nl>
2022-08-22 12:03:14 +0200

    added first Jest test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const linkElement = screen.getByText(/signup/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/pages/home/HomePage.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+import { render, act, screen } from "@testing-library/react";
+
+import HomePage from "./HomePage";
+
+jest.mock("axios");
+
+test("render learn react link", async () => {
+  (axios.get as any).mockImplementation(() =>
+    Promise.resolve({
+      data: {
+        count: 2,
+        rows: [
+          makeFakePost(1, "Fake post #1"),
+          makeFakePost(2, "Fake post #2"),
+        ],
+      },
+    }),
+  );
+
+  await act(async () => {
+    render(<HomePage />);
+  });
+  const pageTitleE1 = screen.getByText("Codaisseur Coders Network");
+  expect(pageTitleE1).toBeInTheDocument();
+
+  const postA = screen.getByText("Fake post #2", {
+    selector: "h2",
+  });
+  expect(postA).toBeInTheDocument();
+
+  const postB = screen.getByText("Fake post #2", {
+    selector: "h2",
+  });
+  expect(postB).toBeInTheDocument();
+});
+
+function makeFakePost(id: number, title: string) {
+  return {
+    id,
+    title,
+    content: "Bla bla bla",
+    createdAt: "2020-10-06T14:05:05.976Z",
+    updatedAt: "2020-10-06T14:05:05.258Z",
+    author_id: 2,
+    tags: [],
+    post_likes: [],
+  };
+}

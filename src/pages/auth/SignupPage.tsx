9f6a4f77d5d5bc28a0a4141368be5ab477cfeafb
Tim Maas <tim.maas@conclusion.nl>
2022-08-22 10:40:18 +0200

    scaffolding plus basic Routing

---
+++ src/pages/auth/SignupPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Typography, Container } from "@mui/material";
+
+export default function SignupPage() {
+  return (
+    <Container fixed>
+      <Typography variant="h3" component="h1">
+        Codaisseur Coders Network
+      </Typography>
+      <p>SignUp</p>
+    </Container>
+  );
+}
